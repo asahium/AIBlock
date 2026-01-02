4533eddf9db91bbb55f4d3894335e0fdb41ddbb1
asahium <hiksdante@gmail.com>
2026-01-02 17:10:39 +0100

    Update Astro configuration for consistent base path and add trailing slash; modify favicon link to use BASE_URL for improved routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asahium.github.io',
-  base: '/AIBlock',
+  base: '/AIBlock/',
+  trailingSlash: 'always',
   output: 'static',
   build: {
     assets: 'assets'
